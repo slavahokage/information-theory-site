76442df65decd1ee3828100b578e362f3c515127
slavahokage <v_smirnov@ikantam.com>
2019-09-24 22:00:19 +0300

    add style for welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('body')
+    <link href="{{ asset('css/welcome-style.css') }}" rel="stylesheet">
     <div class="jumbotron" style="margin-top: 8%">
-        <h1 class="display-3">Website about various types of encryption</h1>
+        <h1 id="title"class="display-3">Website about various types of encryption</h1>
         <p class="lead">Using this site, you can encrypt and decrypt files with various types of encryption</p>
         <hr class="my-4">
     </div>
